7e72acc40de42e8d07b7fa2b3704bea2582c7d0d
Junaid Malik <junmalik109@gmail.com>
2023-01-24 17:59:06 +0000

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,22 +2,14 @@ import React from 'react'
 import HalaTourLogo from '../assets/hala-tour-logo.png'
 const Header = () => {
   return (
-    <header className='container'>
-        <div>
-            <img src={HalaTourLogo} alt='Hala Tour Logo' /> 
-            <div>
-                <h1 className=''>Hala Tour</h1>
-                <p>Travel Agency</p>
-            
+    <div>
 
-            </div>
-        </div>
     
 <nav className="bg-white border-gray-200 dark:bg-gray-900">
     <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl px-4 md:px-6 py-2.5">
         <a href="https://flowbite.com" className="flex items-center">
-            <img src="https://flowbite.com/docs/images/logo.svg" className="h-6 mr-3 sm:h-9" alt="Flowbite Logo" />
-            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+            <img src={HalaTourLogo} className="h-6 mr-3 sm:h-9 bg-white" alt="Flowbite Logo" />
+            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Hala Tours</span>
         </a>
         <div className="flex items-center">
             <a href="tel:5541251234" className="mr-6 text-sm font-medium text-gray-500 dark:text-white hover:underline">(555) 412-1234</a>
@@ -49,7 +41,7 @@ const Header = () => {
     </div>
 </nav>
 
-    </header>
+    </div>
   )
 }
 
